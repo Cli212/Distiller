fix mi loss overflow

src/Distiller/glue_preprocess.py
@@ -331,7 +331,7 @@ def load_and_cache_examples(args, tokenizer, mode, return_examples=False, s_toke
     # dataset = convert_features_to_dataset(features, is_training=(mode == 'train'))
     if s_tokenizer:
         if os.path.exists(s_cached_features_file) and not args.overwrite_cache:
-            logger.info("Loading student features from cached file %s", cached_features_file)
+            logger.info("Loading student features from cached file %s", s_cached_features_file)
             s_features = torch.load(s_cached_features_file)
         else:
             logger.info("Creating student features from dataset file at %s", args.data_dir)

src/Distiller/mp_aug.py
@@ -49,7 +49,7 @@ def generate_aug_data(examples, original_dataset, augmenter, args, tokenizer, s_
         )
         aug_examples = list(
             tqdm(
-                p.imap(annotate_, example_iter(examples), chunksize=256),
+                p.imap(annotate_, example_iter(examples)),
                 total=int(len(examples) / 32) + 1,
                 desc="Data augmentation",
                 disable=False,
@@ -71,15 +71,17 @@ def generate_aug_data(examples, original_dataset, augmenter, args, tokenizer, s_
 
     dataset = convert_features_to_dataset(features, s_features)
     new_dataset = ConcatDataset([original_dataset, dataset])
-    if args.local_rank == 0:
-        torch.distributed.barrier()
     return new_dataset
 
 def aug_process(queue:Queue, examples, original_dataset, augmenter, args, tokenizer, s_tokenizer=None):
     while True:
         if queue.empty():
+            if args.local_rank not in [-1, 0]:
+                torch.distributed.barrier()
             new_dataset = generate_aug_data(examples, original_dataset, augmenter, args, tokenizer, s_tokenizer)
             queue.put(new_dataset)
+        if args.local_rank == 0:
+            torch.distributed.barrier()
         else:
             time.sleep(300)
             continue

src/Distiller/textbrewer/distiller_basic.py
@@ -204,7 +204,7 @@ class BasicDistiller(AbstractDistiller):
         for current_epoch in tqdm(range(int(num_epochs)), disable=tqdm_disable):
             if self.t_config.q and current_epoch%5 == 0 and current_epoch != 0:
                 train_dataset = None
-                QUEUE_LIMIT = 60
+                QUEUE_LIMIT = 600
                 count = 0
                 while count < QUEUE_LIMIT:
                     try:
@@ -262,6 +262,9 @@ class BasicDistiller(AbstractDistiller):
                         #     torch.nn.utils.clip_grad_norm_(self.model_S.parameters(), max_grad_norm)
                         # Since the gradients of optimizer's assigned params are unscaled, clips as usual:
                         torch.nn.utils.clip_grad_norm_(self.model_S.parameters(), max_grad_norm)
+                        if self.d_config.critic and self.d_config.baseline_fn:
+                            torch.nn.utils.clip_grad_norm_(self.d_config.critic.parameters(), max_grad_norm)
+                            torch.nn.utils.clip_grad_norm_(self.d_config.baseline_fn.parameters(), max_grad_norm)
 
                     if self.t_config.fp16:
                         scaler.step(optimizer)

src/Distiller/utils.py
@@ -609,8 +609,9 @@ def mlp(in_dim, hidden_size, out_dim):
     return torch.nn.Sequential(
         torch.nn.Linear(in_dim, hidden_size),
         torch.nn.ReLU(),
+        torch.nn.Linear(hidden_size, hidden_size),
+        torch.nn.ReLU(),
         torch.nn.Linear(hidden_size, out_dim),
-        torch.nn.ReLU()
     )
 
 