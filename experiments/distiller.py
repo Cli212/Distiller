fix mixup overflow

experiments/distiller.py
@@ -125,28 +125,40 @@ def train(args, examples, train_dataset, t_model, s_model, tokenizer, augmenter=
         if args.intermediate_loss_type == 'mi':
             from Distiller.utils import mlp_critic
             baseline_fn = mlp_critic(t_model.config.hidden_size if args.local_rank==-1 else t_model.module.config.hidden_size, hidden_size=512, out_dim=1)
+            for name, param in baseline_fn.named_parameters():
+                if 'weight' in name:
+                    torch.nn.init.xavier_uniform(param)
+                    print(name, param.data)
+                elif 'bias' in name:
+                    torch.nn.init.constant_(param, 0)
+                    print(name, param.data)
             baseline_fn.to(args.device)
             critic = mlp_critic(t_model.config.hidden_size if args.local_rank==-1 else t_model.module.config.hidden_size, s_model.config.hidden_size if args.local_rank==-1 else s_model.module.config.hidden_size, 512, 32)
             critic.to(args.device)
             critic_parameters = [
-                {"params": [p for n, p in critic.named_parameters() if not any(nd in n for nd in no_decay)],
+                {"params": [p for n, p in critic.named_parameters()],
                  "weight_decay": args.weight_decay},
-                {"params": [p for n, p in critic.named_parameters() if any(nd in n for nd in no_decay)],
-                 "weight_decay": 0.0},
-                {"params": [p for n, p in baseline_fn.named_parameters() if not any(nd in n for nd in no_decay)],
-                 "weight_decay": args.weight_decay},
-                {"params": [p for n, p in baseline_fn.named_parameters() if any(nd in n for nd in no_decay)],
-                 "weight_decay": 0.0}
+                {"params": [p for n, p in baseline_fn.named_parameters()],
+                 "weight_decay": args.weight_decay}
             ]
             optimizer_grouped_parameters.extend(critic_parameters)
         optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
+
+        def sigmoid_reverse(x):
+            """
+                transform alpha logit from range [0.0, 1.0] to [-5.0,0.0,5.0]
+            """
+            return -np.log(1 / x - 1.)
+        args.alpha = sigmoid_reverse(args.alpha)
         if args.intermediate_strategy and args.intermediate_strategy.lower() == "emd":
             distill_config = DistillationConfig(
                 temperature=args.temperature,
+                hard_label_weight=args.hard_label_weight,
                 kd_loss_weight=args.kd_loss_weight,
                 kd_loss_type=args.kd_loss_type,
                 critic=critic,
-                baseline_fn=baseline_fn)
+                baseline_fn=baseline_fn,
+                alpha=args.alpha)
         else:
             # intermediate_matches = matches
             # if args.intermediate_strategy == "skip":
@@ -157,6 +169,7 @@ def train(args, examples, train_dataset, t_model, s_model, tokenizer, augmenter=
             distill_config = DistillationConfig(
                 temperature=args.temperature,
                 intermediate_matches=matches,
+                hard_label_weight=args.hard_label_weight,
                 kd_loss_weight=args.kd_loss_weight,
                 kd_loss_type=args.kd_loss_type,
                 critic=critic,
@@ -173,7 +186,7 @@ def train(args, examples, train_dataset, t_model, s_model, tokenizer, augmenter=
             from Distiller.adapters import BertForGLUEAdptor as adaptor_func
         adaptor_T = adaptor_func
         adaptor_S = adaptor_func
-        if args.intermediate_strategy == "EMD":
+        if args.intermediate_strategy == "emd":
             distiller = EMDDistiller(train_config=train_config,
                                      distill_config=distill_config,
                                      model_T=t_model, model_S=s_model,
@@ -268,6 +281,8 @@ def main(args):
     ## Training
     if args.train:
         # examples = read_examples_from_file(args.data_dir, mode="train", task_type=args.task_type)
+        if args.local_rank not in [-1, 0]:
+            torch.distributed.barrier()
         matches = cal_layer_mapping(args, t_config, s_config)
         train_dataset, s_dataset, features, s_features, examples = load_and_cache_examples(args, t_tokenizer, mode="train",
                                                                 return_examples=True, s_tokenizer=s_tokenizer)
@@ -298,6 +313,8 @@ def main(args):
                 # train_dataset = generate_aug_data(examples, train_dataset, augmenter, args, t_tokenizer, s_tokenizer)
                 process.start()
                 # process.join()
+        if args.local_rank == 0:
+            torch.distributed.barrier()
         train(args, examples, train_dataset, t_model, s_model, t_tokenizer, augmenter, matches, predict_callback, q=q)
         # p = Process(target=data_aug_process, args=(augmenter,examples,tokenizer,args))
         # p.start()
@@ -366,7 +383,7 @@ def main(args):
 
 if __name__ == '__main__':
     args = parse()
-    set_start_method('spawn')
+    # set_start_method('spawn')
     if args.S_model_name_or_path is None:
         args.S_model_name_or_path = args.T_model_name_or_path
     if args.task_type in ["squad","squad2"]:

src/Distiller/textbrewer/distiller_emd.py
@@ -145,7 +145,11 @@ class EMDDistiller(BasicDistiller):
             f_S = feature_maps_S[s]
             for t in range(feature_num_T):
                 f_T = feature_maps_T[t]
-                distance_matrix[s][t + feature_num_S] = distance_matrix[t + feature_num_S][s] = match_loss(f_S, f_T,
+                if loss_type == 'mi':
+                    distance_matrix[s][t + feature_num_S] = distance_matrix[t + feature_num_S][s] = match_loss(f_S, f_T, self.d_config.critic, self.d_config.baseline_fn,
+                                                   self.d_config.alpha)
+                else:
+                    distance_matrix[s][t + feature_num_S] = distance_matrix[t + feature_num_S][s] = match_loss(f_S, f_T,
                                                                                                            mask=inputs_mask_S)
 
         feature_weight_S = np.concatenate([self.feature_weight_S, np.zeros(feature_num_T)])
@@ -178,7 +182,11 @@ class EMDDistiller(BasicDistiller):
         #    trans_matrix, distance_matrix.detach(), feature_weight_S, feature_weight_T, self.d_config.temperature)
 
         # embedding matching
-        embedding_loss = match_loss(embeddings_S, embeddings_T, mask=inputs_mask_S)
+        if loss_type == 'mi':
+            embedding_loss = match_loss(embeddings_S, embeddings_T, self.d_config.critic, self.d_config.baseline_fn, self.d_config.alpha)
+        else:
+            embedding_loss = match_loss(embeddings_S, embeddings_T, mask=inputs_mask_S)
+        # embedding_loss = match_loss(embeddings_S, embeddings_T, mask=inputs_mask_S)
         total_loss += embedding_loss * emd_loss_weight  # sharing the same weight
         losses_dict[f'unweighted_embedding_{loss_type}'] = embedding_loss
 
@@ -191,9 +199,12 @@ class EMDDistiller(BasicDistiller):
             losses_dict['unweighted_hard_label_loss'] = total_hl_loss
         if 'loss' in results_S:
             total_hl_loss = 0
-            for loss in results_S['loss']:
-                # in case of multi-GPU
-                total_hl_loss += loss.mean()
+            if isinstance(results_S['loss'], list):
+                for loss in results_S['loss']:
+                    # in case of multi-GPU
+                    total_hl_loss += loss.mean()
+            else:
+                total_hl_loss = results_S['loss']
             total_loss += total_hl_loss * self.d_config.hard_label_weight
             losses_dict['unweighted_hard_label_loss'] = total_hl_loss
         return total_loss, losses_dict

src/Distiller/textbrewer/losses.py
@@ -300,7 +300,7 @@ def mi_loss(state_S, state_T, critic, baseline_fn, alpha):
         cls_S = state_S
     log_baseline = torch.squeeze(baseline_fn(y=cls_T))
     scores = critic(cls_S, cls_T)
-    return torch.tensor(1.)-interpolated_lower_bound(scores, log_baseline, alpha)
+    return -interpolated_lower_bound(scores, log_baseline, alpha)
 
 
 def log_prob_gaussian(x):
@@ -309,7 +309,7 @@ def log_prob_gaussian(x):
 
 def reduce_logmeanexp_nodiag(x, axis=None):
     batch_size = x.shape[0]
-    diag_inf = torch.diag(torch.tensor(np.inf) * torch.ones(batch_size)).to("cuda")
+    diag_inf = torch.diag(torch.tensor(np.inf) * torch.ones(batch_size)).to(x.device)
     logsumexp = torch.logsumexp(x - diag_inf, dim=(0,1))
     if axis:
         num_elem = batch_size - 1.
@@ -320,14 +320,27 @@ def reduce_logmeanexp_nodiag(x, axis=None):
 
 def log_interpolate(log_a, log_b, alpha_logit):
     """Numerically stable implementation of log(alpha * a + (1-alpha) * b)."""
-    log_alpha = -torch.nn.functional.softplus(-torch.tensor(alpha_logit))
+    log_alpha = -torch.nn.functional.softplus(torch.tensor(-alpha_logit))
     log_1_minus_alpha = -torch.nn.functional.softplus(torch.tensor(alpha_logit))
     y = torch.logsumexp(torch.stack((log_alpha + log_a, log_1_minus_alpha + log_b),0), dim=0)
     return y
 
 
 def softplus_inverse(x):
-    return torch.log(torch.exp(x) - torch.tensor(1.))
+    # x = x.numpy()
+    threshold = torch.log(torch.tensor(torch.finfo(x.dtype).eps)) + torch.tensor(2.)
+    is_too_small = x < torch.exp(threshold)
+    is_too_large = x > -threshold
+    too_small_value = torch.log(x).type(x.dtype)
+    too_large_value = x
+    # This `where` will ultimately be a NOP because we won't select this
+    # codepath whenever we used the surrogate `ones_like`.
+    x = torch.where(is_too_small | is_too_large, torch.ones([], dtype=x.dtype).to(x.device), x)
+    y = x + torch.log(-torch.expm1(-x)).type(x.dtype)  # == log(expm1(x))
+    return torch.where(is_too_small,
+                    too_small_value,
+                    torch.where(is_too_large, too_large_value, y))
+    # return torch.log(torch.exp(x) - torch.tensor(1.))
 
 
 def compute_log_loomean(scores):
@@ -370,13 +383,13 @@ def interpolated_lower_bound(scores, baseline, alpha_logit):
     interpolated_baseline = log_interpolate(
       nce_baseline, torch.tile(baseline[:, None], (1, batch_size)), alpha_logit)
     # Marginal term.
-    critic_marg = scores - torch.diagonal(interpolated_baseline, offset=0, dim1=-2, dim2=-1)[:, None]
+    critic_marg = scores - torch.diagonal(interpolated_baseline, offset=0)[:, None]
     marg_term = torch.exp(reduce_logmeanexp_nodiag(critic_marg))
 
     # Joint term.
-    critic_joint = torch.diagonal(scores, offset=0, dim1=-2, dim2=-1)[:, None] - interpolated_baseline
+    critic_joint = torch.diagonal(scores, offset=0)[:, None] - interpolated_baseline
     joint_term = (torch.sum(critic_joint) -
-                torch.sum(torch.diagonal(critic_joint, offset=0, dim1=-2, dim2=-1))) / (batch_size * (batch_size - 1.))
+                torch.sum(torch.diagonal(critic_joint, offset=0))) / (batch_size * (batch_size - 1.))
     return torch.tensor(1.) + joint_term  - marg_term
 
 