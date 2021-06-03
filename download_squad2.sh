3f9b6f45afc3a30aefc594444889240df9eaaf55
haoyuhe <hehaoyuh212@gmail.com>
2021-06-04 01:12:46 +0800

    update ray script

--- download_squad2.sh
+++ download_squad2.sh
@@ -1,2 +1,2 @@
-wget -P datasets/squadv2/ https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json
-wget -P datasets/squadv2/ https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v2.0.json
+wget -P ~/Distillation_QA_benchmark/datasets/squadv2/ https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json
+wget -P ~/Distillation_QA_benchmark/datasets/squadv2/ https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v2.0.json
