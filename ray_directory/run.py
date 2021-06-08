811d67d3c1c1373c3045d80fbcc9779aee95df92
haoyuhe <hehaoyuh212@gmail.com>
2021-06-08 17:45:42 +0800

    update ray script

--- ray_directory/run.py
+++ ray_directory/run.py
@@ -280,16 +280,16 @@ def remote_fn(config, checkpoint_dir=None):
             args.__setattr__("task_name", task_name)
             if task_name == 'sst-2':
                 args.__setattr__("T_model_name_or_path", "howey/electra-large-sst2")
-                args.__setattr__("data_dir", "~/Distillation_QA_benchmark/datasets/glue_data/SST-2")
+                # args.__setattr__("data_dir", "~/Distillation_QA_benchmark/datasets/glue_data/SST-2")
             elif task_name == "stsb":
                 args.__setattr__("T_model_name_or_path", "howey/electra-large-stsb")
-                args.__setattr__("data_dir", "~/Distillation_QA_benchmark/datasets/glue_data/STS-B")
+                # args.__setattr__("data_dir", "~/Distillation_QA_benchmark/datasets/glue_data/STS-B")
             elif task_name == "cola":
                 args.__setattr__("T_model_name_or_path", "howey/electra-large-cola")
-                args.__setattr__("data_dir", "~/Distillation_QA_benchmark/datasets/glue_data/CoLA")
+                # args.__setattr__("data_dir", "~/Distillation_QA_benchmark/datasets/glue_data/CoLA")
             else:
                 args.__setattr__("T_model_name_or_path", f"howey/electra-large-{task_name}")
-                args.__setattr__("data_dir", f"~/Distillation_QA_benchmark/datasets/glue_data/{task_name.upper()}")
+                # args.__setattr__("data_dir", f"~/Distillation_QA_benchmark/datasets/glue_data/{task_name.upper()}")
         else:
             raise NotImplementedError
     globals()['best_evaluation'] = 0.0
@@ -563,8 +563,7 @@ def main(args, gpus_per_trial=4):
     # }
 
     search_space = {
-        "s_model": tune.grid_search(list(model_dict.keys())),
-        "task_name": tune.grid_search(glue_list)
+        "s_model": tune.grid_search(list(model_dict.keys()))
     }
     # search_space = {
     #     "intermediate_strategy": tune.grid_search(["skip", "last"]),
@@ -642,7 +641,7 @@ def main(args, gpus_per_trial=4):
 
 
 if __name__ == '__main__':
-    ray.init(address='auto', _redis_password='5241590000000000')
+    ray.init(address='auto', _redis_password='5241590000000000', ignore_reinit_error=True)
     args = parse()
     # set_start_method('spawn')
     if args.S_model_name_or_path is None:
--- ray_directory/run.sh
+++ ray_directory/run.sh
@@ -1,9 +1,9 @@
 
 #set hyperparameters
 #BERT_DIR=output-bert-base/squad_base_cased_lr3e2_teacher
-TEACHER_DIR=howey/electra-large-rte
+TEACHER_DIR=howey/electra-large-mnli
 STUDENT_DIR=huawei-noah/TinyBERT_General_4L_312D
-DATA_ROOT_DIR=~/Distillation_QA_benchmark/datasets/glue_data/RTE
+DATA_ROOT_DIR=~/Distillation_QA_benchmark/datasets/glue_data/MNLI
 OUTPUT_ROOT_DIR=output-student
 
 #STUDENT_CONF_DIR=student_configs/bert_base_cased_L4.json
@@ -23,7 +23,7 @@ length=128
 torch_seed=9580
 hard_label_weight=0.0
 kd_loss_weight=1.0
-task_name=rte
+task_name=mnli
 task_type=glue
 NAME=${TEACHER_DIR}_${STUDENT_DIR}_lr${lr}e-5_e${ep}_${task_type}_${task_name}_${intermediate_strategy}_${intermediate_loss_type}_alpha${alpha}_h${hard_label_weight}_k${kd_loss_weight}_${kd_loss_type}
 OUTPUT_DIR=${OUTPUT_ROOT_DIR}/${NAME}
