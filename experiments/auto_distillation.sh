7c45bc2a28e1a68ffea929b85f085e5e3ca61379
haoyuhe <hehaoyuh212@gmail.com>
2021-07-19 17:45:42 +0800

    auto distiller update

--- experiments/auto_distillation.sh
+++ experiments/auto_distillation.sh
@@ -1,5 +1,12 @@
 #set hyperparameters
 #BERT_DIR=output-bert-base/squad_base_cased_lr3e2_teacher
+export ISENGARD_PRODUCTION_ACCOUNT=false
+export AWS_ACCESS_KEY_ID=ASIA237V3YQYGNJROGGZ
+export AWS_SECRET_ACCESS_KEY=VKLrQANFFLVC7FlYfeG8ylirp+oQJwccjHZUgqP3
+export AWS_SESSION_TOKEN=IQoJb3JpZ2luX2VjEJL//////////wEaCXVzLWVhc3QtMSJGMEQCIGLvJfOmqfp/m4/by9sIxRdYXt8C6dmtWxY6PGWx5/CIAiBfH8EFZtLwhW9pt2xeQskP4PiZ1cKGRio2Fuh7VckjuyqnAgiL//////////8BEAEaDDc0NzMwMzA2MDUyOCIMmCxDfbAVfdO43dV/KvsBrdYMhBdX97Y3Ey4nCn7lRasvNI/h8YUvv/EQY6J9PO4nwArwRTDeZWF5XCrtvsQ71wAVrYfyLmb9wqVB77tLEJQVKfrpfVVqvGMI+/Ir10EW3cy0csou7vT1GTvKj8CmVt9AENWdtmouUbzV8r8K9lT9YbB1f5zGVdKEUdEVHPDqcr/NPpBr0UFJBPNAdrwMe1nAF9onQrh6BMw4U52ZkHMyw38lT41Our8y1G66t44/mKAmSWSsHLNN0scGWYGnNG70xl8BlJ4ZKDDioV/kjgbEGYnpxNpaxh15XVSA21ABcuQengXXhH5W+tKuqJL3ys7yFf5YeM7LWD0whpDVhwY6ngFKW76Muk9F4WA1rgZDjrm0V+dXaRmwyZ0KAuL64PU1m+R+Et5gLXighQNyzAJe0nbKpmZCAZYn/1co2BEWUC+BKM9U20PBs1T6sol7SSWnOj0kNGpq7b5A4gsFs8MQXOSPiQ6ASvdoPxTW/Rsth+oFxL2mqVhtRVIt/8HLUUkUN0wJd7nWJ5iWCiFAGeGeBlhLY7UjnAIxh84pKoD6fw==
+aws s3 cp --recursive s3://haoyu-nlp/auto_mm/women_clothing_review/ ../datasets/auto_mm/women_clothing_review/
+
+
 TEACHER_DIR=howey/bert-base-uncased-cloth
 STUDENT_DIR=huawei-noah/TinyBERT_General_4L_312D
 DATA_ROOT_DIR=../datasets/auto_mm/women_clothing_review/
@@ -7,7 +14,7 @@ OUTPUT_ROOT_DIR=output-student
 
 #STUDENT_CONF_DIR=student_configs/bert_base_cased_L4.json
 accu=1
-ep=20
+ep=10
 lr=5
 alpha=0.9
 #augmenter_config_path=augmenter_config.json
